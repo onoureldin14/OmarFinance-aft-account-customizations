1ec167dbd53f6b46fc33617dd471090545f2ad6f
Omar Din <omardin@Omars-MacBook-Air.local>
2025-07-02 08:04:58 +0100

    adding production costs

--- PRODUCTION/terraform/main.tf
+++ PRODUCTION/terraform/main.tf
@@ -1,7 +1,9 @@
-resource "aws_config_config_rule" "r" {
-  name = "vpc_flow_log_guardrail"
-  source {
-    owner             = "AWS"
-    source_identifier = "VPC_FLOW_LOGS_ENABLED"
-  }
+resource "aws_budgets_budget" "total_cost" {
+  name              = "budget-total-monthly"
+  budget_type       = "COST"
+  limit_amount      = "100"
+  limit_unit        = "USD"
+  time_period_end   = "2087-06-15_00:00"
+  time_period_start = "2022-02-01_00:00"
+  time_unit         = "MONTHLY"
 }
